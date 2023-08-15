79afa3dfa7f951daa6db2f4170bd9f864c2259b8
joel <joel.yacob.teklemariam@nmbu.no>
2023-08-15 14:44:05 +0200

    legger til filene fra Tin100

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,210 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 10:51:21 2023
+
+@author: fbi
+"""
+
+### Importering av moduler
+
 import streamlit as st
+import tin100 as tin
+import pandas as pd
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+## title
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title("""
+          Lånesøknad for boliglån
+    """)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+## Bilde
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.markdown("![Alt Text]("
+            "https://blogg.paretobank.no/hs-fs/hubfs/Driftsfinansiering%20Slik%20skriver%20du%\
+20en%20lånesøknad.png?width=1000&name=Driftsfinansiering%20Slik%20skriver%20du%20en%20lånesøknad.png)")
+
+alder = st.number_input("**Alder**", min_value=0, max_value=100, value=30, step=1, key=1)
+
+gender = st.radio("**Kjønn**", ("Mann", "Dame"), key=2)
+
+Gift = st.selectbox("**Gift**", ["Ja", "Nei"], key=3)
+
+Selvstendig = st.selectbox("**Selvsendig drivende**", ["Ja", "Nei"], key=4,
+                           help="Hvordan driver, henter du inntekt?")
+
+Utdanning = st.selectbox("**Utdanning**", ["Ja", "Nei"], key=5,
+                         help="Utdanning høgre enn vidergåande.")
+
+Barn = st.number_input("**Barn under 18 år**", min_value=0, max_value=100, value=0, step=1, key=6)
+
+if Barn >= 3:
+    Barn = 3
+else:
+    Barn = Barn
+
+Eigendom = st.selectbox("**Eiendomsområde**", ["Storby", "By", "Distrikt Norge"], key=7,
+                        help="Hvilken eigendomsområder er du fra?")
+if Eigendom == 'Storby':
+    Eigendom = 0
+elif Eigendom == 'By':
+    Eigendom = 1
+else:
+    Eigendom = 2
+
+Kredit_hist = st.slider("**Kredit historie**", 0, 1, 1)
+
+Inntekt = st.number_input("**Inntekt (Antall 1000)**", min_value=0.0, max_value=10000000.0, step=5.0, value=50.0, key=8,
+                          help="Her snakker vi om bruttoinntekt (inntekt før skatt)")
+
+Medsokerinntekt = st.number_input("**Medsøkerinntekt (Antall 1000)**", min_value=0.0, max_value=10000000.0,
+                                  step=5.0, value=50.0, key=9,
+                                  help="Medsøkerinntekt er inntekten til en person som søker sammen med deg om å få et "
+                                       "lån eller annen form for finansiering. Dette kan være en ektefelle, samboer "
+                                       "eller "
+                                       " noen annen form for partner.")
+
+tid_laan = st.number_input("**Tidligere lån? (antall 1000)**", min_value=0.0, max_value=10000000.0, step=5.0,
+                           value=50.0,
+                           key=10, help="Hvor mye tidligere lån har du?")
+
+onsk_laan = st.number_input("**Lån (antall 1000)**", min_value=0.0, max_value=10000000.0, step=5.0, value=50.0,
+                            key=11, help="Hvor mye vil du låne?")
+
+egenkapital = st.number_input("**Egenkapital (antall 1000)**", min_value=0.0, max_value=10000000.0, step=1.0,
+                              value=5.0, key=12, help="Hvor mye egenkapital har du?")
+
+mnd = st.slider("**Låne lengde (antall måned)**", 0, 360, 1, help="Hvor lang tid vil du låne?", key=13)
+
+rente = st.slider("**Rente**", min_value=0.0, max_value=10.0, step=0.1, key=14,
+                  help='Sjekk ut nebetaligsplan for ulike renter.')
+
+Laan = tid_laan + onsk_laan  # totalt lån, tidligere + ønsket lån
+
+if rente < 1.04:
+    rente = 1.07
+else:
+    rente += 0.03
+
+Laan_med_rente = Laan * (1 + rente / 100) ** (mnd / 12)  # lån med hensyn til rente og måneder
+
+### Legger dataen inn i en dictionary
+
+data_dic = {'Gender': [np.where(gender == 'Male', 1, 0)],
+            'Married': [np.where(Gift == 'Ja', 1, 0)],
+            'Dependents': [Barn],
+            'Education': [np.where(Utdanning == 'Ja', 1, 0)],
+            'Self_Employed': [np.where(Selvstendig == 'Ja', 1, 0)],
+            'ApplicantIncome': [Inntekt * 1000],
+            'CoapplicantIncome': [Medsokerinntekt * 1000],
+            'LoanAmount': [Laan * 1000],
+            'Loan_Amount_Term': [tid_laan * 1000],
+            'Credit_History': [Kredit_hist],
+            'Property_Area': [Eigendom]}
+
+## Konverterer fra dictionary til dataframe
+data = pd.DataFrame.from_dict(data_dic)
+
+## Tekst med info om nettsiden
+with st.sidebar:
+    st.subheader('Om siden')
+    st.markdown('En lånesøknad er en formell anmodning om å få låne penger fra en bank eller annen kredittinstitusjon.'
+                ' Søknaden skal inneholde informasjon om den personen som søker lån, for eksempel inntekt, formue,'
+                ' gjeld og personlig informasjon. Søknaden skal også inneholde informasjon om hva lånet skal brukes'
+                ' til, samt den ønskede lånebeløpet og løpetiden.')
+
+    st.markdown('Det er viktig at all informasjonen som gis i en lånesøknad er korrekt og oppdatert, siden bankene'
+                ' eller kredittinstitusjonene vil bruke denne informasjonen for å vurdere søkerens kredittverdighet'
+                ' og avgjøre om lånesøknaden skal godkjennes eller ikke.')
+
+    st.markdown('Det er også viktig å sjekke og sammenligne ulike tilbud og lånebetingelser fra forskjellige'
+                ' institusjoner før en avgjørelse tas. Så i korte trekk så er en lånesøknad en formell anmodning'
+                ' om lån fra en institusjon som skal inneholde informasjonen om deg som person, dine økonomiske'
+                ' forhold og hva lånet skal brukes til.')
+    st.markdown('Denne nettsiden er lagd av **Joel**, **Dino** and **Trish** ved hjelp av modulen Streamlit')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+### Nedbetalings graph
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+## Total lån med rente, nedbetalig av lån, fast betaling til toalat lån med rente (totalt kostnad)
 
-    points_per_turn = total_points / num_turns
+def Laan_graph(graph=None):
+    G = Laan * 1000
+    terminrente = ((rente / 100) / (1 - (1 + (rente / 100)) ** (-mnd)))
+    y = G * terminrente  # Måndelig terminbeløp
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    tot = G * (1 + rente / 100) ** (mnd / 12)
+    terminbelop = tot / mnd
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    nedbetaling = []
+    avdrag = []
+    laanekost = []
+
+    for j in range(mnd + 1):
+        G = G * (1 + rente / 100) - y
+        nedbetaling.append(G)
+        avdrag.append(Laan * 1000 - G)
+        laanekost.append(terminbelop * j)
+
+    chart_data = pd.DataFrame(
+        [[i, k, t] for i, k, t in zip(nedbetaling, avdrag, laanekost)],
+        columns=['Nedbetalingsplan', 'Avdrag på lånesum', 'Total terminbeløp'])
+
+    if graph == 1:
+        st.line_chart(chart_data)
+    else:
+        return np.around(y, 2)
+
+
+Laan_graph(graph=1)
+
+### Informasjon under grafen om lånesøknaden
+
+var = [Laan, Laan, Laan_graph(graph=2), egenkapital]
+
+var_str = ['Totalt lån',
+           'Totalt lån med rente', 'Månedlig terminbeløp', 'Egenkapital krav for lån']
+
+for i, k in zip(var, var_str):
+    if k == 'Låne mengde' or k == 'Totalt lån':
+        i = i * 1000
+    elif k == 'Totalt lån med rente':
+        i = np.around(Laan_med_rente * 1000, 1)
+    elif k == 'Egenkapital krav for lån':
+        i = np.around((onsk_laan * 1000) * 0.15, 1)
+    st.write('**' + k + '**', ': ', i)
+
+
+### knapp som sender lånesøknaden til testing/predikering
+
+def knapp():
+    if st.button('Send søknad'):
+        if onsk_laan * 0.15 >= egenkapital:
+            return st.write('Takk for søknaden, din søknad er dessverre ikke akseptert.'
+                            ' \
+        For å få det ønsket lånet må du ha en egenekapital større eller lik {2} Du kan ikke låne: \
+        {0} USD i {1} måneder'.format(Laan * 1000, mnd, (onsk_laan * 1000) * 0.15))
+
+        elif (Inntekt + Medsokerinntekt) <= (onsk_laan / 5):
+            return st.write('Takk for søknaden, din søknad er dessverre ikke akseptert.'
+                            ' \
+For å få det ønsket lånet må du ha en total inntekt (medsøkerinntekt + inntekt) større eller lik {2} Du kan ikke låne: \
+{0} USD i {1} måneder'.format(Laan * 1000, mnd, (onsk_laan * 1000) / 5))
+
+        prediction = tin.predic(data, tin.RanForClf())
+
+        if prediction == 0:
+            return st.write('Takk for søknaden, din søknad er dessverre ikke akseptert. '
+                            ' \
+                          Du kan ikke låne: {0} USD i {1} måneder'.format(Laan * 1000, mnd))
+        else:
+            return st.write('Takk for søknaden, din søknad er akseptert.'
+                            '\
+         Totalkostnad på {0} år: {1} '
+                            '\
+         , Lånekostnad {2} pr. md.'.format(int(mnd / 12), np.around(Laan_med_rente * 1000, 1),
+                                           np.around(Laan_graph(), 1)))
+
+
+knapp()
---
+++ tin100.py
@@ -0,0 +1,101 @@
+### Importering av moduler
+
+import pandas as pd
+from sklearn.impute import SimpleImputer
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+### Split datasett i train og test
+
+train_raw = pd.read_csv("train.csv")
+test_raw = pd.read_csv("test.csv")
+train = train_raw.drop(['Loan_ID'], axis=1)
+test = test_raw.drop(['Loan_ID'], axis=1)
+
+simp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+
+
+def add_values(var):
+    for col in var.columns:
+        simp_1 = simp.fit(var[[f'{col}']])
+        var[f'{col}'] = simp_1.transform(var[[f'{col}']])
+    return var
+
+
+train = add_values(train)
+test = add_values(test)
+
+le = LabelEncoder()
+for col in train[
+    ['Gender', 'Married', 'Education', 'Self_Employed', 'Dependents', 'Property_Area', 'Credit_History',
+     'Loan_Status']]:
+    # print(col)
+    train[col] = le.fit_transform(train[col])
+
+train['CoapplicantIncome'] = train['CoapplicantIncome'].astype('int')
+# train.head()
+
+le = LabelEncoder()
+for col in test[
+    ['Gender', 'Married', 'Education', 'Self_Employed', 'Dependents', 'Credit_History', 'Property_Area']]:
+    # print(col)
+    test[col] = le.fit_transform(test[col])
+
+test['CoapplicantIncome'] = test['CoapplicantIncome'].astype('int')
+
+x = train.drop('Loan_Status', axis=1)
+y = train['Loan_Status']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, stratify=y)
+
+
+### Randomforest classifier
+
+def RanForClf():
+    parameter_grid = {
+        'n_estimators': [200, 300, 400],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [5, 6, 7, 8],
+        'criterion': ['gini', 'entropy']
+    }
+    rfc = RandomForestClassifier(random_state=42)
+    CV_rfc = GridSearchCV(estimator=rfc, param_grid=parameter_grid, cv=5, scoring='f1', n_jobs=-1, verbose=1)
+    CV_rfc.fit(x, y)
+    return CV_rfc
+
+
+def predic(x, model):
+    return model.predict(x)
+
+
+### Predict of the whole datasett
+if __name__ == "__main__":
+
+    model = RanForClf()
+    pred = model.predict(test)
+
+    test['Loan_Status'] = pred
+
+    ### Creating a new column with 5* total income < loan
+
+    New_loan_status = []
+    for i in range(len(test)):
+        if test.iloc[i]['Loan_Status'] == 0:
+            # print('hei')
+            income = 5 * 12 * (test.iloc[i]['ApplicantIncome'] + test.iloc[i]['CoapplicantIncome'])
+            loan = (test.iloc[i]['LoanAmount']) * 1000
+            New_loan_status.append(income < loan)
+        else:
+            New_loan_status.append(1)
+
+    test['new_loan_status'] = New_loan_status
+
+    df = test['Loan_Status']
+    df.value_counts()
+
+    df = test['new_loan_status']
+    df.value_counts()
